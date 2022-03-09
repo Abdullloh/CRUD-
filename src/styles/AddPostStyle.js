5d6436f23221c99d2d253d7c0917b5a8d2997756
Abdullloh <abdullohjonirgashev@gmail.com>
2022-03-09 23:15:08 +0500

    addpost ui completed

--- src/styles/AddPostStyle.js
+++ src/styles/AddPostStyle.js
@@ -21,6 +21,12 @@ const AddPostStyle = styled.div`
         outline:none;
       }
     }
+    @media (max-width:768px){
+      width:80%;
+    }
+  }
+  @media(max-width:768px){
+    width:100%;
   }
 `;
 
